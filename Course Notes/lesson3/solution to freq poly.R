b78e59f2186dc63b7348416e944bd388372348d1
mrandrewandrade <andrew@andrewandrade.ca>
2014-03-23 21:56:39 -0700

    removed unnescessary files

--- Course Notes/lesson3/solution to freq poly.R
+++
@@ -1,7 +0,0 @@
-pf <- read.csv('pseudo_facebook.tsv', sep = '\t')
-library(ggplot2)
-
-qplot(x=www_likes,data = subset (pf, !is.na(gender)),
-       binwidth = 10, geom = 'freqpoly', color = gender) +
-  scale_x_continuous()
-
